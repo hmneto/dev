843149a50c45a4b36df2414c2f95f50500e07e5e
hmnet <hmnet@HMNETON-NOTE>
2020-05-18 16:13:24 -0300

    update

---
+++ Exemplos/socket-io-basics-master/index.js
@@ -0,0 +1,14 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+app.get('/', function(req, res){
+    res.sendFile(`${__dirname}/index.html`)
+})
+io.on('connection', function(socket){
+    console.log(`usuario ${socket.id} conectado`)
+    socket.on('mensagem', function(data){
+        console.log(data)
+        socket.broadcast.emit('mensagem', `broadcast ${socket['id']}`)
+    })
+})
+http.listen(80)
--- zzzznovo-projeto-c-/promisefy-node-master/index.js
+++
@@ -1,5 +0,0 @@
-let promisify = require('promisify-node')
-let fs = require('fs')
-let readFile = promisify(fs.readFile)
-
-readFile('index.js').then(data=> console.log(data.toString()))
