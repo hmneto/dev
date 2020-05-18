9ff5e4ef468f9194782878f22389e26963b065a6
hmnet <hmnet@HMNETON-NOTE>
2020-05-18 12:27:44 -0300

    update

--- zzzznovo-projeto-c-/imagetext/index.js
+++ zzzznovo-projeto-c-/imagetext/index.js
@@ -5,12 +5,12 @@ var imagetext = require("./imagetext");
 (function() {
 
     var textItems = {
-        northeast: "",
-        northwest: "NorthWest",
-        southeast: "This is the SouthEast",
-        southwest: "This is the southern and western item",
-        center: "In the middle"
+    //     northeast: "",
+    //     northwest: "NorthWest",
+    //     southeast: "This is the SouthEast",
+    //     southwest: "This is the southern and western item",
+    //     center: "In the middle"
     };
-    imagetext.addAllText("c:/temp/1.JPG", "c:/temp/1-modified.JPG", textItems, "#f60");
+    imagetext.addAllText("./img/200.JPG", "./img/201.JPG", textItems, "#f60");
 
 })();
