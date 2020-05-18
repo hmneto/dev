ffe3ebdce1a5eeeef7f96e4665cafdf1b878d4d4
hmnet <hmnet@HMNETON-NOTE>
2020-05-18 10:48:23 -0300

    update

--- Exemplos/stream-arquivos/readable.js
+++ Exemplos/stream-arquivos/readable.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 const readable = fs.createReadStream(`${__dirname}\\teste.js`)
 let count = 0
-//readable.on('data', function(data){
-//    console.log(count++, data)
-//    readable.pause()
-//    setTimeout(function(){
-//        readable.resume()
-//    },10000)
-//})
+readable.on('data', function(data){
+   console.log(count++, data)
+   readable.pause()
+   setTimeout(function(){
+       readable.resume()
+   },10000)
+})
 
 readable.on('readable', function(){
     while(chunk = readable.read()){
