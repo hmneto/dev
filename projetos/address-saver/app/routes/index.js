ba74e760e0c6df69620e742366eefb3772db646e
hmnet <hmnet@HMNETON-NOTE>
2020-06-03 04:17:33 -0300

    axios

--- projetos/address-saver/app/routes/index.js
+++ projetos/address-saver/app/routes/index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const Axios = require('axios')
+
+const url = 'http://localhost:3000'
+// const url = "http://192.168.99.100"
+
+const api = Axios.create({
+    baseURL:url
+})
+
+
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send('ok2123u')
+  api.get('/a/').then(data=>console.log(data.data))
+  res.send('indexl')
 });
 
 module.exports = router;
