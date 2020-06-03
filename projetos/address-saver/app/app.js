368dda944717b29c215b38ca4a23af1365f089cf
hmnet <hmnet@HMNETON-NOTE>
2020-06-03 03:51:00 -0300

    update

--- projetos/address-saver/app/app.js
+++ projetos/address-saver/app/app.js
@@ -20,7 +20,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/a', function(req,res){
+  res.send('nova okl')
+
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- projetos/address-saver/app/routes/index.js
+++ projetos/address-saver/app/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send('ok2')
+  res.send('ok2123u')
 });
 
 module.exports = router;
