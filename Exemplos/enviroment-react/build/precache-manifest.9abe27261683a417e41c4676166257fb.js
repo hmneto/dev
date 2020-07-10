21e1246c0d64020f819ff4a2b01dc3cb312954d7
hmnet <hmnet@HMNETON-NOTE>
2020-07-10 17:20:14 -0300

    update

---
+++ Exemplos/enviroment-react/build/precache-manifest.9abe27261683a417e41c4676166257fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29fd5595527baf33a75692db6439f3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a1c614cd8b7af5248a8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b0ac3f454f3a775ba1c3",
+    "url": "/static/js/2.9df2829a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9df2829a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1c614cd8b7af5248a8",
+    "url": "/static/js/main.73d23323.chunk.js"
+  },
+  {
+    "revision": "334bc887324152744294",
+    "url": "/static/js/runtime-main.8ca5ca62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
