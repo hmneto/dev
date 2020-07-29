bf1806e67c69ab4db3e37b2f01abd6543a3b8315
root <hmneton@gmail.com>
2020-07-29 08:25:45 -0300

    update

---
+++ Exemplos/flexbox/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+export default () =>
+    <>
+        <div className="container">
+            <div className="item">1</div>
+            <div className="item">2</div>
+            <div className="item">3</div>
+            <div className="item">4</div>
+        </div>
+    </>
--- init.sh
+++ init.sh
@@ -6,3 +6,42 @@ sudo apt install build-essential default-jdk libssl-dev exuberant-ctags ncurses-
 
 
 . $HOME/.asdf/asdf.sh
+
+
+ sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-` uname -s `-` uname -m` -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo chown $USER /var/run/docker.sock
+
+
+$ sudo service docker stop
+$ sudo mv /var/lib/docker /var/lib/docker.bak
+$ sudo service docker start
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+docker-compose --version
+
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install openssh-client
+
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install openssh-client
+
+sudo apt-get install ubuntu-restricted-extras
+
+
+cat /etc/apt/sources.list
+
+
+https://github.com/artf/grapesjs.git
