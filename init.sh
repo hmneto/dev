235851cc5adea8be8ca00fe3255219a5903f6441
Honório <hmneton@gmail.com>
2020-08-18 13:31:38 -0300

    update

--- init.sh
+++ init.sh
@@ -58,3 +58,28 @@ cat /etc/apt/sources.list
 
 
 https://github.com/artf/grapesjs.git
+
+
+apt update && apt upgrade -y && apt install php -y && apt install nodejs -y && apt install git -y && npm install nodemon -g && npm install create-react-app -g && npm install yarn -g && npm install create-react-native-app -g && git config --global user.email "hmneton@gmail.com" && git config --global user.name "hmneto" && git clone https://github.com/hmneto/dev.git && cd dev && cd web && npm install && cd .. && cd android && npm install && npm start
+
+[1/16, 2:57 AM] Honório Neto: pkg install wget proot -y
+[1/16, 2:58 AM] Honório Neto: mkdir ubuntu
+[1/16, 2:59 AM] Honório Neto: bash ./ubuntu.sh
+[1/16, 3:00 AM] Honório Neto: wget https://raw.githubusercontent.com/Neo-Oli/termux-ubuntu/master/ubuntu.sh
+[1/16, 3:03 AM] Honório Neto: ./start-ubuntu.sh
+
+https://portal.azure.com/#blade/Microsoft_Azure_Education/EducationMenuBlade/software
+
+É ra@ises.com.br e a senha q vc usa para acessar a área do aluno
+
+sudo apt install gedit -y &&
+sudo gedit /etc/apt/sources.list &&
+sudo apt install docker.io -y && 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && 
+sudo chmod +x /usr/local/bin/docker-compose && 
+sudo apt install git -y &&
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+sudo dpkg -i google-chrome-stable_current_amd64.deb &&
+sudo apt-add-repository ppa:nemh/systemback &&
+sudo apt-get update &&
+sudo apt-get install systemback -y
