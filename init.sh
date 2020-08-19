67008e967c39f65fcde8bbefec8439ba5a023873
hmneto <hmneton@gmail.com>
2020-08-19 12:01:57 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,76 +1,5 @@
-sudo apt update && sudo apt upgrade -y && sudo apt install git -y && sudo apt install docker.io -y &&sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose && docker-compose --version && git config --global user.email "hmneton@gmail.com" && git config --global user.name "Honório"
 
 
-
-git config --global user.email "hmneton@gmail.com" && git config --global user.name "Honório" &&
-
-
-cd ~/Desktop/codes/dev/ && git add . && git commit -m "update" && git push
-
-
-
-
-cd ~/Desktop/codes/ && git clone git@github.com:hmneto/dev
-
-
-sudo apt install virtualbox
-
-sudo apt install build-essential default-jdk libssl-dev exuberant-ctags ncurses-term ack-grep silversearcher-ag fontconfig imagemagick libmagickwand-dev software-properties-common git vim-gtk3 curl -y
-
-
-. $HOME/.asdf/asdf.sh
-
-
- sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-` uname -s `-` uname -m` -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-sudo chown $USER /var/run/docker.sock
-
-
-$ sudo service docker stop
-$ sudo mv /var/lib/docker /var/lib/docker.bak
-$ sudo service docker start
-
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-
-docker-compose --version
-
-
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install openssh-client
-
-
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install openssh-client
-
-sudo apt-get install ubuntu-restricted-extras
-
-
-cat /etc/apt/sources.list
-
-
-https://github.com/artf/grapesjs.git
-
-
-apt update && apt upgrade -y && apt install php -y && apt install nodejs -y && apt install git -y && npm install nodemon -g && npm install create-react-app -g && npm install yarn -g && npm install create-react-native-app -g && git config --global user.email "hmneton@gmail.com" && git config --global user.name "hmneto" && git clone https://github.com/hmneto/dev.git && cd dev && cd web && npm install && cd .. && cd android && npm install && npm start
-
-[1/16, 2:57 AM] Honório Neto: pkg install wget proot -y
-[1/16, 2:58 AM] Honório Neto: mkdir ubuntu
-[1/16, 2:59 AM] Honório Neto: bash ./ubuntu.sh
-[1/16, 3:00 AM] Honório Neto: wget https://raw.githubusercontent.com/Neo-Oli/termux-ubuntu/master/ubuntu.sh
-[1/16, 3:03 AM] Honório Neto: ./start-ubuntu.sh
-
-https://portal.azure.com/#blade/Microsoft_Azure_Education/EducationMenuBlade/software
-
-É ra@ises.com.br e a senha q vc usa para acessar a área do aluno
 
 sudo apt install gedit -y &&
 sudo gedit /etc/apt/sources.list &&
@@ -82,4 +11,64 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &
 sudo dpkg -i google-chrome-stable_current_amd64.deb &&
 sudo apt-add-repository ppa:nemh/systemback &&
 sudo apt-get update &&
-sudo apt-get install systemback -y
+sudo apt-get install systemback -y &&
+git config --global user.email "hmneton@gmail.com" && 
+git config --global user.name "Honório" &&
+sudo apt install virtualbox -y
+
+
+
+cd ~/Desktop/codes/dev/ && git add . && git commit -m "update" && git push
+
+
+
+
+cd ~ && git clone git@github.com:hmneto/dev
+
+
+
+sudo apt install build-essential default-jdk libssl-dev exuberant-ctags ncurses-term ack-grep silversearcher-ag fontconfig imagemagick libmagickwand-dev software-properties-common git vim-gtk3 curl -y
+
+
+. $HOME/.asdf/asdf.sh
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo chown $USER /var/run/docker.sock
+
+$ sudo service docker stop
+$ sudo mv /var/lib/docker /var/lib/docker.bak
+$ sudo service docker start
+
+sudo apt-get install openssh-client
+
+sudo apt-get install ubuntu-restricted-extras
+
+
+apt update && 
+apt upgrade -y && 
+apt install php -y && 
+apt install nodejs -y && 
+apt install git -y && 
+npm install nodemon -g && 
+npm install create-react-app -g && 
+npm install yarn -g && npm install create-react-native-app -g && 
+git config --global user.email "hmneton@gmail.com" && 
+git config --global user.name "hmneto" && 
+git clone https://github.com/hmneto/dev.git && 
+cd dev && cd web && npm install && 
+cd .. && 
+cd android && 
+npm install && 
+npm start
+
+[1/16, 2:57 AM] Honório Neto: pkg install wget proot -y
+[1/16, 2:58 AM] Honório Neto: mkdir ubuntu
+[1/16, 2:59 AM] Honório Neto: bash ./ubuntu.sh
+[1/16, 3:00 AM] Honório Neto: wget https://raw.githubusercontent.com/Neo-Oli/termux-ubuntu/master/ubuntu.sh
+[1/16, 3:03 AM] Honório Neto: ./start-ubuntu.sh
+
+https://portal.azure.com/#blade/Microsoft_Azure_Education/EducationMenuBlade/software
+
+É ra@ises.com.br e a senha q vc usa para acessar a área do aluno
+
