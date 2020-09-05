58a49c873b91d2e54bf6da30abed9c0cc90b71bb
root <hmneton@gmail.com>
2020-09-05 10:01:52 -0300

    update

--- ok/CreateUserController.php
+++
@@ -1,4 +0,0 @@
-<?php
-require 'IpOptionsClass.php';
-$r = new IpOptions();
-echo $r->getIp();
--- ok/TController.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: *');
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit(0);
-}
-
-echo file_get_contents("http://api/TController.php");
