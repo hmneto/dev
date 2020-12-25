59c89624e179d50cba30aff29f88db4759bbafbf
honorio <hmneton@gmail.com>
2020-12-25 00:48:04 +0000

    update

--- docker-java-hello-world/HelloWorld.java
+++ docker-java-hello-world/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println(" - java.version: " + System.getProperty("java.version"));
-        System.out.println(" - java.vm.version: " + System.getProperty("java.vm.version"));
+        System.out.println("teste java");
+        //System.out.println(" - java.version: " + System.getProperty("java.version"));
+        //System.out.println(" - java.vm.version: " + System.getProperty("java.vm.version"));
     }
 }
