40d4d80d320bd491fd6c4ab0ae785a2d76e15701
hmneto <hmneton@gmail.com>
2021-01-04 00:48:25 -0300

    Update init.sh

--- zip/init.sh
+++ zip/init.sh
@@ -1,6 +1,5 @@
 git clone https://github.com/hmneto/dev
-git clone https://github.com/hmneto/dev
 cd ~/dev/zip
 docker build -t teste .
-docker run -d --name novo -t teste 
+docker run -d --rm --name novo -t teste 
 docker logs -f novo
