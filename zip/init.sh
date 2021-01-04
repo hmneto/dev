c8a5127deb9e1431c9ce56fd7c7638016125486f
hmneto <hmneton@gmail.com>
2021-01-04 00:39:32 -0300

    Update init.sh

--- zip/init.sh
+++ zip/init.sh
@@ -1,10 +1,8 @@
 git clone https://github.com/hmneto/dev
-git pull
 cd ~/dev/zip
+git pull
 docker image remove teste
 docker rm -f novo
 docker build -t teste .
-docker run -d --name novo -t teste 
-docker logs -f novo
 docker run -d --rm --name novo -t teste 
 docker logs -f novo
