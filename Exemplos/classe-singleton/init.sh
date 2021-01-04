451f6aadba8f3ccff8d73a8fa860dd9a0d0779b6
hmneto <hmneton@gmail.com>
2021-01-04 01:48:28 -0300

    Update init.sh

--- Exemplos/classe-singleton/init.sh
+++ Exemplos/classe-singleton/init.sh
@@ -2,5 +2,5 @@ git clone https://github.com/hmneto/dev
 cd ~/dev/Exemplos/classe-singleton/
 docker rm -f novo
 docker build -t teste .
-docker run -d --rm --name novo -t teste 
+docker run -it --rm --name novo -t teste 
 docker logs -f novo
