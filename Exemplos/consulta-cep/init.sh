45f28c5a0e6d86c53e31e1b6346b9f83b6675a24
hmneto <hmneton@gmail.com>
2021-01-04 01:51:07 -0300

    Update init.sh

--- Exemplos/consulta-cep/init.sh
+++ Exemplos/consulta-cep/init.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/hmneto/dev
-cd ~/dev/Exemplos/classe-singleton/
+cd ~/dev/Exemplos/consulta-cep/
 docker rm -f novo
 docker build -t teste .
 docker run -it --rm --name novo -t teste
