d68d080b6f0b4e850437a3d956a9043aa281d325
hmneto <hmneton@gmail.com>
2021-01-04 00:31:02 -0300

    Update script_init.sh

--- Exemplos/auto-pull-git-node/script_init.sh
+++ Exemplos/auto-pull-git-node/script_init.sh
@@ -1,16 +1,11 @@
 
 git clone https://github.com/hmneto/dev
-cd ~/dev/Exemplos/auto-pull-git-node/
-docker build -t teste .
-docker run -d --name novo -t teste 
-docker logs -f novo
-
-docker-compose up --build
-
 git pull
 cd ~/dev/Exemplos/auto-pull-git-node/
 docker image remove teste
 docker rm -f novo
 docker build -t teste .
+docker run -d --name novo -t teste 
+docker logs -f novo
 docker run -d --rm --name novo -t teste 
 docker logs -f novo
