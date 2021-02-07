30699b091ca1680be58795bd36509e81a92c003a
hmneto <hmneton@gmail.com>
2021-02-07 05:22:20 -0300

    Delete tiu-termux_install_ubuntu.sh

--- scripts/tiu-termux_install_ubuntu.sh
+++
@@ -1,6 +0,0 @@
-apt update && 
-apt install wget proot -y && 
-mkdir ubuntu && 
-wget https://raw.githubusercontent.com/Neo-Oli/termux-ubuntu/master/ubuntu.sh && 
-bash ./ubuntu.sh && 
-./start-ubuntu.sh
