e7cdfa7904274ba110820c729db1aed32a76afc8
hmneto <hmneton@gmail.com>
2021-02-07 04:51:26 -0300

    Delete index.js

--- webpack_build (1)/index.js
+++
@@ -1,8 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    console.log('Welcome to production');
-  }
-  if (process.env.DEBUG) {
-    console.log('Debugging output');
-  }
-console.log('ok')
-  
