76d8ab4e4acccfe52086b47b3ead4db4f7ffb164
hmneto <hmneton@gmail.com>
2021-02-07 04:13:19 -0300

    Delete script.sh

--- docker-java-hello-world/script.sh
+++
@@ -1,9 +0,0 @@
-docker run -it -v $(pwd):/build openjdk:8u131-jdk-alpine javac /build/HelloWorld.java
-
-docker build -t hello-world:8u131 -f Dockerfile-8u131 .
-
-docker run -it --rm=true hello-world:8u131
-
-docker build -t hello-world:9b170 -f Dockerfile-9b170 .
-
-docker run -it --rm=true hello-world:9b170
